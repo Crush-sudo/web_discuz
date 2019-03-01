1770a7405bd4e9156a6fe6faa536fee61ed4a21e
songxuedd <736836975@qq.com>
2019-03-01 13:34:15 +0800

    first add

--- testsuites/All_test.py
+++ testsuites/All_test.py
@@ -1,10 +1,17 @@
 import HTMLTestRunner
 import unittest
 import os
+import sys
+sys.path.append('E:\\PyCharm\\Discuz')
 from testsuites.test_login_post_reoly import test_login_post_reoly
 from testsuites.test_del_mes_and_newmodule import test_del_mes_and_newmodule
 from testsuites.test_search_mes import test_search_mes
 from testsuites.test_release_vote import test_release_vote
+# print(sys.path)
+# os.chdir('E:\\PyCharm\\Discuz')
+# #打印出项目路径下的目录
+# for file in os.listdir(os.getcwd()):
+#      print(file)
 #构造存储路径
 report_path=os.path.dirname(os.path.abspath("."))+"/report/"
 if not os.path.exists(report_path):os.mkdir(report_path)
